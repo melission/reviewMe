458b980dffece299fb7cc698da6fd08f841ea5fb
Polina <polina@blsv.space>
2021-05-05 16:04:04 +0300

    add an index func to render main page with 'index.html' and context as a dictionary

--- reviewMe/views.py
+++ reviewMe/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('index page')
+    # http://127.0.0.1:8000/?name=True shows True page
+    # http://127.0.0.1:8000/?name=    [with spaces] is being considered as an empty str
+    # http://127.0.0.1:8000/?name='       ' turned out as name=''
+    name = request.GET.get("name") or "index"
+    # return HttpResponse(f'{name} page')
+    return render(request, 'index.html', {'name': name})
