bba8cabed7799d7f511cd31b24e0f74014dd3acc
Polina <polina@blsv.space>
2021-05-06 15:48:44 +0300

    add a path 'search' which works with 'def book_search'

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import books_all
+from .views import *
 
 urlpatterns = [
-    path('', books_all)
+    path('', books_all),
+    path('search/', book_search),
 ]
