4804cb72e2d7725b9403d93e538f8c89d05533cf
Polina <polina@blsv.space>
2021-05-06 15:48:03 +0300

    add 'def book_search'

--- books/views.py
+++ books/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def books_all(request):
-    return HttpResponse('Books list')
+    return render(request=request, template_name='base.html')
+
+
+def book_search(request):
+    # http://127.0.0.1:8000/books/search/?search_phrase=%27another%20good%20book%20to%20read%27
+    search = request.GET.get("search_phrase") or 'one marvelous book'
+    return render(request, 'search_result_books.html', {'search_phrase': search})
