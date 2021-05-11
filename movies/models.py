4f8070b0a4e07559f44950f474ab70598ad64c9c
Polina <polina@blsv.space>
2021-05-11 14:06:13 +0300

    add the model class for movies

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=64)
+    # poster = models.ImageField()
+    released_at = models.IntegerField()
+    description = models.CharField(max_length=1000)
+
