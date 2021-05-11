515f80a700f6239a088631602109f83ca0190343
Polina <polina@blsv.space>
2021-05-11 14:38:21 +0300

    add review and isbn fields to the class Book

--- books/models.py
+++ books/models.py
@@ -10,4 +10,6 @@ class Book(models.Model):
     published_at = models.IntegerField(help_text='The year the book was published')
     publisher_name = models.CharField(max_length=64, help_text='Publisher name')
     description = models.CharField(max_length=1000)
+    review = models.TextField
+    isbn - models.CharField(max_length=20, verbose_name='ISBN number of the book')
 
