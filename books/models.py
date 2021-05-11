405d128e53b0dd028e84984c5d8bdafb88c6b24a
Polina <polina@blsv.space>
2021-05-11 14:06:00 +0300

    add the model class for books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=64,
+                             help_text='The title of the book', blank=False)
+    # cover = models.ImageFiled()
+    published_at = models.IntegerField(help_text='The year the book was published')
+    publisher_name = models.CharField(max_length=64, help_text='Publisher name')
+    description = models.CharField(max_length=1000)
+
