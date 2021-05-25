f5ed6890a3a62ede278a48590a29b10474a4f11d
Polina <polina@blsv.space>
2021-05-25 16:53:44 +0300

    a very beginning of creation a class-based view

--- books/views.py
+++ books/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def books_all(request):
-    return render(request=request, template_name='base.html')
+# def books_all(request):
+#     return render(request=request, template_name='base.html')
 
+class BookHomePage(TemplateView):
+    template_name = 'books.html'
 
 def book_search(request):
     # http://127.0.0.1:8000/books/search/?search_phrase=%27another%20good%20book%20to%20read%27
     search = request.GET.get("search_phrase") or 'one marvelous book'
     return render(request, 'search_result_books.html', {'search_phrase': search})
+
