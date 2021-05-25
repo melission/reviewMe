8198c0982e0569dc979fc3126f3a04508ef14242
Polina <polina@blsv.space>
2021-05-25 17:42:57 +0300

    modify: 1. with ORM 'book_count' now show how many books has added 2. render the info

--- books/views.py
+++ books/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from .models import *
 
 
 # Create your views here.
-# def books_all(request):
-#     return render(request=request, template_name='base.html')
+def books_all(request):
+    book_count = Book.objects.count()
+    return render(request=request, context={'context': f"there are {book_count} books has found"},
+                  template_name='books.html')
 
-class BookHomePage(TemplateView):
-    template_name = 'books.html'
+# class BookHomePage(TemplateView):
+#     template_name = 'books.html'
 
 def book_search(request):
     # http://127.0.0.1:8000/books/search/?search_phrase=%27another%20good%20book%20to%20read%27
