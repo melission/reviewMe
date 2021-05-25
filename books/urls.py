53ab26e455c4e32ad2cc808dc405634ef18daee5
Polina <polina@blsv.space>
2021-05-25 16:54:44 +0300

    add a class-based view BookHomePage to urlpatterns

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', books_all),
+    # path('', books_all),
+    path('', BookHomePage),
     path('search/', book_search),
 ]
