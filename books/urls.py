e1aba08dfb0449322be876687530f55e7f007d8a
Polina <polina@blsv.space>
2021-05-25 17:41:28 +0300

    back to work with function-based view [books_all]

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', books_all),
-    path('', BookHomePage.as_view()),
+    path('', books_all),
+    # path('', BookHomePage.as_view()),
     path('search/', book_search),
 ]
