b4bc367a70499120873f9838a4a792fbcdadffe0
Polina <polina@blsv.space>
2021-05-27 20:33:36 +0300

    created + added def book_list_page to calculate the average rating of a book

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from books.models import Book, Review
+from .utils import average_rating
 # Create your views here.
+
+
+def book_list_page(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = Review.objects.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book': book,
+                          'book_rating': book_rating,
+                          'number_of_reviews': number_of_reviews})
+    context = {'book_list': book_list}
+    return render(request, 'book_list.html')
