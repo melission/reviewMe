e9a43d457ab19e65716a1ee03a25eb992147e4ce
Polina <polina@blsv.space>
2021-06-01 17:08:26 +0300

    modify: now if a book has more that one author, all co-authors will be passed to the HTML

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from books.models import Book, Review, BookContributor
 from .utils import average_rating
 # Create your views here.
@@ -9,7 +10,12 @@ def book_list_page(request):
     book_list = []
     for book in books:
         reviews = Review.objects.all()
-        author = book.contributors.filter(bookcontributor__role='AUTHOR')
+        authors = []
+        contributors = book.contributors.filter(Q(bookcontributor__role='AUTHOR') | Q(bookcontributor__role='CO_AUTHOR'))
+        # for i in range(len(contributors)):
+        #     author = contributors[i]
+        #     authors.append(author)
+
         if reviews:
             book_rating = average_rating([review.rating for review in reviews])
             number_of_reviews = len(reviews)
@@ -20,6 +26,7 @@ def book_list_page(request):
                           'book_rating': book_rating,
                           'number_of_reviews': number_of_reviews,
                           'publisher': book.publisher,
-                          'author': author[0]})
+                          'authors': contributors,
+                          })
     context = {'book_list': book_list}
     return render(request, 'book_list.html', context=context)
