5affcb60708d4ab9dd1c2b50d5b2e49745f3a5f7
Polina <polina@blsv.space>
2021-06-02 15:13:13 +0300

    modify: now index return render not HttpResponse

--- reviewMe/views.py
+++ reviewMe/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from books.models import *
+from movies.models import *
+from templates import *
+
 
 def index(request):
     # http://127.0.0.1:8000/?name=True shows True page
     # http://127.0.0.1:8000/?name=    [with spaces] is being considered as an empty str
     # http://127.0.0.1:8000/?name='       ' turned out as name=''
-    name = request.GET.get("name") or "index"
+    # name = request.GET.get("name") or "index"
     # return HttpResponse(f'{name} page')
-    return render(request, 'index.html', {'name': name})
+    # return render(request, 'index.html', {'name': name})
+    book_amount = Book.objects.count()
+    movie_amount = Movie.objects.count()
+    massage = f'On the site {book_amount} titles of books and {movie_amount} names of movies'
+    return render(request, context={'message': massage}, template_name='index.html')
