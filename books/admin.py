add5abf45411d70e4c16ebf12dc715c2901ee2d7
Polina <polina@blsv.space>
2021-06-09 15:22:58 +0300

    modify: add BookAdmin class with two params (title and published_at) for representation on the admin panel; add BookAdmin for admin.site.register

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import *
+from reviewMe.admin import *
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_at')
+
 
 admin.site.register(Publisher)
 admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
