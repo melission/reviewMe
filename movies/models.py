eb3401539db66d4ac94c5c6a2223ce498507acf3
Polina <polina@blsv.space>
2021-06-17 15:21:30 +0300

    corrected all models, now they inherit from django.db.models, as previously they were acting just as python classes; added attribute '.choices' for al three [Director, Actor and Writer] Role models; some misprints were corrected

--- movies/models.py
+++ movies/models.py
@@ -13,52 +13,55 @@ class Movie(models.Model):
     writers = models.ManyToManyField('Writers', through='MovieWriters')
 
 
-class Directors:
+class Directors(models.Model):
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
 
-class MovieDirectors:
+class MovieDirectors(models.Model):
     class DirectorRole(models.TextChoices):
         director = 'DIRECTOR', 'Director'
         co_director = 'ONE OF THE DIRECTORS', 'One of the directors'
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     director = models.ForeignKey(Directors, on_delete=models.CASCADE)
-    role = models.CharField(choices=DirectorRole)
+    role = models.CharField(choices=DirectorRole.choices,
+                            max_length=20)
 
 
-class Actors:
+class Actors(models.Model):
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
 
-class MovieActors:
-    class ActorRole:
-        main_actor = 'MAIN ACTOR', 'Main actor'
-        supporting_role = 'SUPPORTING ROLE', 'Supporting role'
+class MovieActors(models.Model):
+    class ActorRole(models.TextChoices):
+        main_actor = 'MAIN_ACTOR', 'Main_actor'
+        supporting_role = 'SUPPORTING_ROLE', 'Supporting_role'
         # for crowd seen only else supporting role
         other = 'OTHER', 'Other'
         # for cameo only
         cameo = 'CAMEO', 'Cameo'
-    movie = models.ForeignKey(Actors, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     actor = models.ForeignKey(Actors, on_delete=models.CASCADE)
-    role = models.CharField(choices=ActorRole,
+    role = models.CharField(choices=ActorRole.choices,
                             verbose_name='Supporting role for everyone apart main actors, '
-                                         'for crowd seen actors use "other"')
+                                         'for crowd seen actors use "other"',
+                            max_length=20)
 
 
-class Writers:
+class Writers(models.Model):
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
 
-class MovieWriters:
-    class WriterRole:
+class MovieWriters(models.Model):
+    class WriterRole(models.TextChoices):
         author = 'AUTHOR', 'Author'
         co_author = 'CO_AUTHOR', 'Co_author'
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     writer = models.ForeignKey(Writers, on_delete=models.CASCADE)
-    role = models.CharField(choices=WriterRole)
+    role = models.CharField(choices=WriterRole.choices,
+                            max_length=20)
