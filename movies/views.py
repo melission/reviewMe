e6d3e6a6ea8eaeca8a4ea584acad53c152f1cd66
Polina <polina@blsv.space>
2021-06-18 18:59:48 +0300

    finished 'movies_all' function, now passes 'movie_amount' and 'movie_list'

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import *
 from django.http import HttpResponse
 
 
 # Create your views here.
 def movies_all(request):
-    return render(request, 'movie_list.html')
+    movie_amount = Movie.objects.count()
+    movie_list = Movie.objects.all()
+    return render(request,
+                  context={'movie_amount': movie_amount, 'movie_list': movie_list},
+                  template_name='movie_list.html')
