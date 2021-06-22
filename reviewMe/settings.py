fb0fe5d34ff8367ac1ca0e51eb4b855f1944fd14
Polina <polina@blsv.space>
2021-06-22 14:20:52 +0300

    add STATIC_ROOT; changed STATICFILES_DIRS from os.join to 3.1 vDjango version style [ DIR / 'folder']

--- reviewMe/settings.py
+++ reviewMe/settings.py
@@ -143,8 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# collectstatic
+STATIC_ROOT = BASE_DIR / 'production_static'
 # must be a tuple of a list
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/Users/polinabelousova/PycharmProjects/reviewMe/reviewMe/static']
+STATICFILES_DIRS = [BASE_DIR / 'static', '/Users/polinabelousova/PycharmProjects/reviewMe/reviewMe/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
