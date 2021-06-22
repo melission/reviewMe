79460eb56b41008996f6ef77fa90bd3a7e9168e3
Polina <polina@blsv.space>
2021-06-22 13:52:12 +0300

    misprint correction

--- reviewMe/settings.py
+++ reviewMe/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# must be a tuple of a list. IMPORTANT: eed change a path to general static in production
+# must be a tuple of a list
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/Users/polinabelousova/PycharmProjects/reviewMe/reviewMe/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
