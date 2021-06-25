1d5bde285921621ee7f434370489cd67810c40d4
Polina <polina@blsv.space>
2021-06-25 13:39:40 +0300

    all models were included in admin.site.register

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Movie)
+admin.site.register(Actors)
+admin.site.register(MovieActors)
+admin.site.register(Directors)
+admin.site.register(MovieDirectors)
+admin.site.register(Writers)
+admin.site.register(MovieWriters)
