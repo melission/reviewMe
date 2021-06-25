71f5d2b76d9cd9996b6a6d79b6e60dd504972551
Polina <polina@blsv.space>
2021-06-25 15:10:31 +0300

    create ActorAdmin class [list_display, list_filter] attributes; add the class to admin.side.register

--- movies/admin.py
+++ movies/admin.py
@@ -3,8 +3,13 @@ from .models import *
 # Register your models here.
 
 
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    list_filter = ('last_name', 'first_name')
+
+
 admin.site.register(Movie)
-admin.site.register(Actors)
+admin.site.register(Actors, ActorAdmin)
 admin.site.register(MovieActors)
 admin.site.register(Directors)
 admin.site.register(MovieDirectors)
