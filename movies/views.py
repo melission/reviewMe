04aa5b6259cd24fbba1d13e10d10eeefd7890381
Polina <polina@blsv.space>
2021-07-02 14:02:41 +0300

    the order of variables was changed [nothing else]

--- movies/views.py
+++ movies/views.py
@@ -7,6 +7,5 @@ from django.http import HttpResponse
 def movies_all(request):
     movie_amount = Movie.objects.count()
     movie_list = Movie.objects.all()
-    return render(request,
-                  context={'movie_amount': movie_amount, 'movie_list': movie_list},
-                  template_name='movie_list.html')
+    return render(request, 'movie_list.html',
+                  context={'movie_amount': movie_amount, 'movie_list': movie_list})
