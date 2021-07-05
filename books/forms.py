627d435d816e6547e1642b6e07f0d971fdd06d81
Polina <polina@blsv.space>
2021-07-05 11:57:15 +0300

    create a SearchForm and choices where to look for

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from .models import Review
+from django import forms
 
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ['content', 'rating']
+
+
+
+choices = (('BOOK', 'Search a book'), ('Movie', 'Search a movie'), ('Contributor', 'Search an author'),
+           ('Director', 'Search a director'))
+
+class SearchForm(forms.Form):
+    search = forms.CharField(min_length=3, widget=forms.Textarea)
+    search_in = forms.ChoiceField(choices=choices, widget=forms.RadioSelect)
