6f9a0b448f9761aa7c4c5e38fc34534d8b2c1f72
Polina <polina@blsv.space>
2021-07-07 15:41:20 +0300

    removed 'search' path [due to relocation]

--- books/urls.py
+++ books/urls.py
@@ -5,7 +5,7 @@ from reviews.views import book_list_page
 urlpatterns = [
     # path('', books_all),
     # path('', BookHomePage.as_view()),
-    path('search/', book_search),
+    # path('search/', book_search),
     # path('rating/', book_list_page),
     path('', book_list_page),
     path('rating/', book_list_page),
