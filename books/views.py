5bd47249ffd026090728d3f0f5824ce9f9395035
Polina <polina@blsv.space>
2021-07-07 15:43:03 +0300

    remove 'def book_search' [relocation to main app, rename to 'searchField']

--- books/views.py
+++ books/views.py
@@ -30,38 +30,3 @@ def detailed_book_view(request, id):
             pass
         print(form.cleaned_data)
         return render(request, context={'book': book, 'form': form}, template_name='detailed_book_view.html')
-
-
-def book_search(request):
-    # http://127.0.0.1:8000/books/search/?search_phrase=%27another%20good%20book%20to%20read%27
-    search = request.GET.get("search_phrase") or 'one marvelous book'
-    form = SearchForm(request.GET)
-    search_in = request.GET.get('search_in')
-    print(f'search_in {search_in}')
-    search_result = []
-    if search_in is None:
-        pass
-    elif search_in == 'Book':
-        try:
-            search_result.append([Book.objects.get(title__icontains=search)])
-        except Book.DoesNotExist:
-            search_result = [f'There is no book with the name {search}']
-    elif search_in == 'Contributor':
-        # try:
-        #     search_result.append([Contributor.objects.get(last_name__contains=search)])
-        # except Contributor.DoesNotExist:
-        #     pass
-        # try:
-        #     search_result.append([Contributor.objects.get(first_name__contains=search)])
-        #     print(search_result)
-        # except Contributor.DoesNotExist:
-        #     search_result = [f'There is no author with the name {search}']
-        try:
-            result = Contributor.objects.filter(
-                Q(last_name__contains=search) | Q(first_name__contains=search))
-            search_result = [x for x in result]
-        except Contributor.DoesNotExist:
-            search_result.append(f'There is no author named {search}')
-    return render(request, 'search_result_books.html',
-                  {'form': form, 'search_phrase': search, 'search_result': search_result})
-
