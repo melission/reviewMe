e68922e1f1fd9f102c33f550a6045488fd5f27ac
Polina <polina@blsv.space>
2021-07-13 12:07:27 +0300

    created detailed_movie, returns a movie that specified by ID

--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,7 @@ def movies_all(request):
     movie_list = Movie.objects.all()
     return render(request, 'movie_list.html',
                   context={'movie_amount': movie_amount, 'movie_list': movie_list})
+
+def detailed_movie(request, id):
+    movie = Movie.objects.get(id=id)
+    return render(request, 'detailed_movie.html', context={'movie': movie})
