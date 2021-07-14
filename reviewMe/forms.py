33c409ba75d95bbd331d97a7406455968e1a41ac
Polina <polina@blsv.space>
2021-07-14 14:34:25 +0300

    cleated a fun to clean user's input of special symbols [not working atm]

--- reviewMe/forms.py
+++ reviewMe/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 choices = (('Book', 'Search a book'), ('Movie', 'Search a movie'), ('Contributor', 'Search an author'),
            ('Director', 'Search a director'))
 
+
 class SearchForm(forms.Form):
     search_phrase = forms.CharField(min_length=3)
     search_in = forms.ChoiceField(choices=choices, widget=forms.RadioSelect, required=False)
+
+    # clean search phrase from special symbols
+    def clean_search_phrase(self):
+        print('cleaning')
+        req = self.cleaned_data['search_phrase']
+        clean_req = re.sub(r'[?|$|.|!]', r'', req)
+        if clean_req != req:
+            raise ValidationError(f'No special symbols allowed')
+        return clean_req
