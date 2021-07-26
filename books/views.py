7602467cb65bb41fc5729a788c102e996f9461c9
Polina <polina@blsv.space>
2021-07-26 16:53:48 +0300

    from django.contrib import messages

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.db.models import Q
