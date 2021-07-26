1b82098b1e4625d8e908358544e0d413fa13cd29
Polina <polina@blsv.space>
2021-07-26 16:51:04 +0300

    add 'email_on_save' field to PublisherForm, it's commented.

--- books/forms.py
+++ books/forms.py
@@ -3,6 +3,8 @@ from .models import *
 
 
 class ReviewForm(forms.ModelForm):
+    # email_on_save = forms.BooleanField(required=False, help_text="Send a notification if the review is published")
+
     class Meta:
         model = Review
         fields = ['content', 'rating']
