2fa75b0e7906e154f24b913d7486dfdb3d39e385
Polina <polina@blsv.space>
2021-07-27 13:11:00 +0300

    created 'publisher_edit' function

--- books/views.py
+++ books/views.py
@@ -20,10 +20,14 @@ def books_all(request):
 
 
 def detailed_book_view(request, id):
+
+    # get a book based on id
     if request.method == 'GET':
         book = Book.objects.get(id=id)
         form = ReviewForm()
         return render(request, context={'book': book, 'form': form}, template_name='detailed_book_view.html')
+
+    # add a review
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         book = Book.objects.get(id=id)
@@ -31,3 +35,33 @@ def detailed_book_view(request, id):
             pass
         print(form.cleaned_data)
         return render(request, context={'book': book, 'form': form}, template_name='detailed_book_view.html')
+
+
+# a function that gets a request and either saves data to a new publisher, or retrieves due to p_id an existing one
+# p_id is an optional argument; if p_id is None, a new Publisher will be created
+def publisher_edit(request, p_id=None):
+
+    # trys to retrieve an existing Publisher based on p_id
+    if p_id is not None:
+        publisher = get_object_or_404(Publisher, id=p_id)
+    else:
+        publisher = None
+
+    # creates a new Publisher or gets existing one
+    if request.method == 'POST':
+        form = PublisherForm(request.POST, instance=publisher)
+        print(form)
+        if form.is_valid():
+            updated_publisher = form.save()
+            pub = Publisher.objects.get(name=updated_publisher)
+            print(updated_publisher.id)
+            if publisher is None:
+                messages.success(request, f'Publisher {updated_publisher} was created.')
+            else:
+                messages.success(request, f'Publisher {updated_publisher} was updated.')
+
+            return redirect("publisher_edit", pub.id)
+    else:
+        form = PublisherForm(instance=publisher)
+
+    return render(request, 'publisher_edit.html', {'method': request.method, 'form': form})
