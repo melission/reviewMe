8c458aea2bcab7d696cbdba370044c0112754a86
Polina <polina@blsv.space>
2021-08-02 14:14:35 +0300

    add if not form.is_valid [redirect to main page]; yet it is not used now, return all entries instead.

--- reviewMe/views.py
+++ reviewMe/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Q
 from books.models import *
 from movies.models import *
 from templates import *
 from .forms import SearchForm
 from itertools import chain
+from django.contrib import messages
 
 
 def index(request):
@@ -46,69 +47,72 @@ def searchField(request):
     # http://127.0.0.1:8000/books/search/?search_phrase=%27another%20good%20book%20to%20read%27
     # search = request.GET.get("search_phrase") or 'one marvelous book'
     form = SearchForm(request.GET)
+    if not form.is_valid():
+        messages.error(request, 'At least one letter is required.')
+        return redirect('main_page')
     if form.is_valid():
         # print(form.cleaned_data)
         search = form.cleaned_data['search_phrase']
-    search_in = request.GET.get('search_in')
-    search_result = []
-    # print(f'search phrase is {search}')
-    # print(f'search_in {search_in}')
-    if search_in is None:
-        books = bookSearch(search)
-        movies = movieSearch(search)
-        authors = contribSearch(search)
-        directors = directorSearch(search)
+        search_in = request.GET.get('search_in')
+        search_result = []
+        # print(f'search phrase is {search}')
+        # print(f'search_in {search_in}')
+        if search_in is None:
+            books = bookSearch(search)
+            movies = movieSearch(search)
+            authors = contribSearch(search)
+            directors = directorSearch(search)
+            # print(search_result)
+            search_result = list(chain(books, movies, authors, directors))
+        if search_in == 'Book':
+            search_result = bookSearch(search)
+            # try:
+            # except Book.DoesNotExist:
+            #     search_result = [f'There is no book with the name {search}']
+        if search_in == 'Contributor':
+            search_result = contribSearch(search)
+            # try:
+            #     search_result.append([Contributor.objects.get(last_name__contains=search)])
+            # except Contributor.DoesNotExist:
+            #     pass
+            # try:
+            #     search_result.append([Contributor.objects.get(first_name__contains=search)])
+            #     print(search_result)
+            # except Contributor.DoesNotExist:
+            #     search_result = [f'There is no author with the name {search}']
+            # try:
+            # except Contributor.DoesNotExist:
+            #     search_result.append(f'There is no author named {search}')
+        if search_in == 'Movie':
+            search_result = movieSearch(search)
+        if search_in == 'Director':
+            search_result = directorSearch(search)
         # print(search_result)
-        search_result = list(chain(books, movies, authors, directors))
-    if search_in == 'Book':
-        search_result = bookSearch(search)
-        # try:
-        # except Book.DoesNotExist:
-        #     search_result = [f'There is no book with the name {search}']
-    if search_in == 'Contributor':
-        search_result = contribSearch(search)
-        # try:
-        #     search_result.append([Contributor.objects.get(last_name__contains=search)])
-        # except Contributor.DoesNotExist:
-        #     pass
-        # try:
-        #     search_result.append([Contributor.objects.get(first_name__contains=search)])
-        #     print(search_result)
-        # except Contributor.DoesNotExist:
-        #     search_result = [f'There is no author with the name {search}']
-        # try:
-        # except Contributor.DoesNotExist:
-        #     search_result.append(f'There is no author named {search}')
-    if search_in == 'Movie':
-        search_result = movieSearch(search)
-    if search_in == 'Director':
-        search_result = directorSearch(search)
-    # print(search_result)
 
-    # # if QuerySey is empty, key is saved with a str as value
-    # for key in result.keys():
-    #     # print(len(result[key].values()))
-    #     if len(result[key].values()) > 0:
-    #         # length = len(result[key].values())
-    #         # count = 0
-    #         search_result[key] = [value for value in result[key].values()]
-            # for value in result[key]:
-            #     search_result[count] = value
-            #     count+=1
-            #     print(value)
+        # # if QuerySey is empty, key is saved with a str as value
+        # for key in result.keys():
+        #     # print(len(result[key].values()))
+        #     if len(result[key].values()) > 0:
+        #         # length = len(result[key].values())
+        #         # count = 0
+        #         search_result[key] = [value for value in result[key].values()]
+                # for value in result[key]:
+                #     search_result[count] = value
+                #     count+=1
+                #     print(value)
 
-    # for entry in result:
-    #     if result[entry] is None:
-    #         pass
-    #     else:
-    #         search_result[entry] = result[entry]
+        # for entry in result:
+        #     if result[entry] is None:
+        #         pass
+        #     else:
+        #         search_result[entry] = result[entry]
 
-    # if len(result) > 0:
-    #     search_result = [x for x in result]
-    # if len(result) > 0:
-    #     search_result = [x for x in result]
-    # elif len(result) == 0:
-    #     search_result[result] = [f'Nothing has been found on {search}']
-    print(search_result)
-    return render(request, 'search_result.html',
-                  {'form': form, 'search_phrase': search, 'search_result': search_result})
+        # if len(result) > 0:
+        #     search_result = [x for x in result]
+        # if len(result) > 0:
+        #     search_result = [x for x in result]
+        # elif len(result) == 0:
+        #     search_result[result] = [f'Nothing has been found on {search}']
+        print(search_result)
+        return render(request, 'search_result.html',
+                      {'form': form, 'search_phrase': search, 'search_result': search_result})
