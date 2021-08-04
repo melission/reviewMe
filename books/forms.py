75d623e74bac074ce87b01dbcc35ec1919362bc3
Polina <polina@blsv.space>
2021-08-04 15:13:58 +0300

    remove ReviewForm model; remove import Review from review app.

--- books/forms.py
+++ books/forms.py
@@ -1,16 +1,5 @@
 from django import forms
 from .models import *
-from reviews.models import Review
-
-
-class ReviewForm(forms.ModelForm):
-    # email_on_save = forms.BooleanField(required=False, help_text="Send a notification if the review is published")
-
-    class Meta:
-        model = Review
-        fields = ['content', 'rating']
-        widgets = {'content': forms.Textarea(attrs={'placeholder': 'Write your review here'})}
-    rating = forms.IntegerField(min_value=0, max_value=5)
 
 
 class PublisherForm(forms.ModelForm):
