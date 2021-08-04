a25527e41aa5d92728505af3f2c45c58c6e29b87
Polina <polina@blsv.space>
2021-08-04 15:15:18 +0300

    add import 'book_list_edit' [review app]; add a path for adding review.

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-# from reviews.views import book_list_page
+from reviews.views import book_review_edit
 
 urlpatterns = [
     # path('', books_all),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', book_list_page),
     path('rating/', book_list_page),
     path('details/<int:id>/', detailed_book_view),
+    path('details/<int:id>/add_review/', book_review_edit),
     path('publishers/<int:p_id>/', publisher_edit, name='publisher_edit'),
     path('publishers/new/', publisher_edit, name='publisher_create'),
 ]
