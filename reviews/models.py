57de02263638e391dcbaa677a293443205762651
Polina <polina@blsv.space>
2021-08-04 15:12:31 +0300

    created; moved ReviewBook (renamed from Review) from books application to review application.

---
+++ reviews/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib import auth
+from books.models import Book
+# Create your models here.
+
+
+class ReviewBook(models.Model):
+    content = models.TextField(help_text='The review text.')
+    rating = models.IntegerField(help_text='The rating the reviewer has given')
+    created_at = models.DateTimeField(auto_now_add=True, help_text='The date and time the review was created',
+                                      editable=False)
+    edited_at = models.DateTimeField(null=True, help_text='The date and time the review was edited for the last time',
+                                     editable=False)
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, editable=False, on_delete=models.CASCADE, help_text='The book the review for')
+
