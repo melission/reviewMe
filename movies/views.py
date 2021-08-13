219c9b47a5da50788a05e8ed1b4a606d9044252d
Polina <polina@blsv.space>
2021-08-13 15:23:15 +0300

    add m2m filters for movie_list

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 from django.http import HttpResponse
 
 
@@ -10,14 +11,22 @@ def movies_all(request):
     movie_list = []
 
     for movie in movies:
+        directors = movie.directors.filter(
+            Q(moviedirectors__role='DIRECTOR') | Q(moviedirectors__role='ONE OF THE DIRECTORS'))
+        actors = movie.actors.filter(
+            Q(movieactors__role='MAIN_ACTOR') | Q(movieactors__role='SUPPORTING_ROLE')
+        )
+        writers = movie.writers.filter(
+            Q(moviewriters__role='AUTHOR') | Q(moviewriters__role = 'CO_AUTHOR')
+        )
         movie_list.append({
             'id': movie.id,
             'title': movie.title,
             'released_at': movie.released_at,
-            'actors': movie.actors,
-            'directors': movie.directors,
+            'actors': actors,
+            'directors': directors,
             'description': movie.description,
-            'writers': movie.writers,
+            'writers': writers,
         })
 
     return render(request, 'movie_list.html',
