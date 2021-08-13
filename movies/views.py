8a044a463b06994824344c6f26f44798b52a7e99
Polina <polina@blsv.space>
2021-08-13 14:54:40 +0300

    rewrite 'movies_all' function, how it gives more information.

--- movies/views.py
+++ movies/views.py
@@ -5,10 +5,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def movies_all(request):
-    movie_amount = Movie.objects.count()
-    movie_list = Movie.objects.all()
+    # movie_amount = Movie.objects.count()
+    movies = Movie.objects.all()
+    movie_list = []
+
+    for movie in movies:
+        movie_list.append({
+            'id': movie.id,
+            'title': movie.title,
+            'released_at': movie.released_at,
+            'actors': movie.actors,
+            'directors': movie.directors,
+            'description': movie.description,
+            'writers': movie.writers,
+        })
+
     return render(request, 'movie_list.html',
-                  context={'movie_amount': movie_amount, 'movie_list': movie_list})
+                  context={'movie_list': movie_list})
 
 def detailed_movie(request, id):
     movie = Movie.objects.get(id=id)
