9f49c5cd9604dba76d88dc77231ba5fb96c4d800
Polina <polina@blsv.space>
2021-08-16 17:36:15 +0300

    added def clean for last and first name.

---
+++ movies/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import *
+
+
+class AddActorForm(forms.ModelForm):
+    class Meta:
+        model = Actors
+        fields = '__all__'
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data['last_name']
+        last_name = last_name.title()
+        return last_name
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        first_name = first_name.title()
+        return first_name
+
