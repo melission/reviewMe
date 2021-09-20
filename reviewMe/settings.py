afaf11e1fdf527d97f6cb3ec99c5870d6f377545
Polina <polina@blsv.space>
2021-09-20 19:00:40 +0300

    fixed media_root, now it returns a correct path not a list.

--- reviewMe/settings.py
+++ reviewMe/settings.py
@@ -157,5 +157,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static',
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = [BASE_DIR / 'media']
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 MEDIA_URL = '/media/'
