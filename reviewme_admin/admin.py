33f6b25cce3054aeaa44e1acdf3a670d6dcd0c0c
Polina <polina@blsv.space>
2021-11-01 16:22:24 +0300

    added a new instance 'admin_site'.

--- reviewme_admin/admin.py
+++ reviewme_admin/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 # Register your models here.
 class ReviewMeAdmin(admin.AdminSite):
     site_header = 'ReviewMe // Administration Side'
+
+
+admin_site = ReviewMeAdmin(name='reviewme_admin')
