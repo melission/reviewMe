c8bb5551ef4a2e094dc94d144936013a1fe3292b
Polina <polina@blsv.space>
2021-11-02 14:44:08 +0300

    rewrote ReviewmeAdminConfig class by adding a new attribute 'default_site'.

--- reviewme_admin/apps.py
+++ reviewme_admin/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
+# the child class that overwrites 'default_site' properties and allows to register all models.
 class ReviewmeAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reviewme_admin'
+    # default_auto_field = 'django.db.models.BigAutoField'
+    # name = 'reviewme_admin'
+    default_site = 'reviewme_admin.admin.ReviewMeAdmin'
