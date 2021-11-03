0f8cfbcef4b1136fa16369bb4c03ac6a7d706db7
Polina <polina@blsv.space>
2021-11-03 14:05:18 +0300

    added a new class 'ReviewmeAdminConfig'.

--- reviewme_admin/apps.py
+++ reviewme_admin/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
 
 
 # the child class that overwrites 'default_site' properties and allows to register all models.
-class ReviewmeAdminConfig(AppConfig):
+class ReviewmeAdminConfig(AdminConfig):
     # default_auto_field = 'django.db.models.BigAutoField'
     # name = 'reviewme_admin'
     default_site = 'reviewme_admin.admin.ReviewMeAdmin'
