8a61a4a499ba01052ce13a6f2756470a6cb33eed
Polina <polina@blsv.space>
2021-11-04 10:59:33 +0300

    added 'logout_template' attribute.

--- reviewme_admin/admin.py
+++ reviewme_admin/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 # Register your models here.
 class ReviewMeAdmin(admin.AdminSite):
     site_header = 'ReviewMe // Administration Side'
+    logout_template = 'admin/logout.html'
 
 
 admin_site = ReviewMeAdmin(name='reviewme_admin')
