a5e5acdfd525d59d375c9fea42669f3e9d270378
Polina <polina@blsv.space>
2021-11-05 15:27:23 +0300

    added 'profile_view' function inside ReviewMeAdmin class.

--- reviewme_admin/admin.py
+++ reviewme_admin/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
 
 # Register your models here.
 class ReviewMeAdmin(admin.AdminSite):
     site_header = 'ReviewMe // Administration Side'
     logout_template = 'admin/logout.html'
+
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, 'admin/admin_profile.html', context)
 
 
 admin_site = ReviewMeAdmin(name='reviewme_admin')
