beaffaf1219e7400fb54a0885a50ad97a2f88255
Polina <polina@blsv.space>
2021-12-01 19:00:02 +0300

    added the logic to 'reviews_list_tag'.

--- reviews/templatetags/profile_tags.py
+++ reviews/templatetags/profile_tags.py
@@ -3,7 +3,22 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('reviews_list.html', takes_context=True)
-def reviews_list_tag(context):
-    print(context)
-    pass
+
+@register.inclusion_tag('reviews_list.html')
+def reviews_list_tag(user_id):
+    # print(user_id)
+    raw_reviews = ReviewBook.objects.filter(creator_id=user_id)
+    reviews = []
+    for review in raw_reviews:
+        reviews.append(
+            {
+                'content': review.content,
+                'rating': review.rating,
+                # 'created_at': review.created_at.date(),
+                'book_id': review.book.id,
+                'book_title': review.book.title,
+
+            }
+        )
+    # print(reviews)
+    return {"reviews": reviews}
