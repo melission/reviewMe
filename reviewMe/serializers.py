689209245b3bd8b3422ec0ddc6fb779333040bd2
Polina <polina@blsv.space>
2021-12-08 18:25:23 +0300

    created: PublisherSerializer and BookSerializer classes.

--- reviewMe/serializers.py
+++ reviewMe/serializers.py
@@ -1,1 +1,16 @@
 from rest_framework import serializers
+
+
+class PublisherSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    website = serializers.URLField()
+
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    publisher = PublisherSerializer()
+    isbn = serializers.CharField()
+    published_at = serializers.DateTimeField()
+    description = serializers.CharField()
