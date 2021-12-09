9bafd47e6220c54ddce2c788f7022ac4f11c1265
Polina <polina@blsv.space>
2021-12-09 13:12:25 +0300

    rewrote PublisherSerializer and BookSerializer using Class Based Views; added books.models import.

--- reviewMe/serializers.py
+++ reviewMe/serializers.py
@@ -1,16 +1,26 @@
 from rest_framework import serializers
+from books.models import *
 
 
-class PublisherSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    website = serializers.URLField()
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = '__all__'
+    # id = serializers.IntegerField()
+    # name = serializers.CharField()
+    # website = serializers.URLField()
 
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField()
+class BookSerializer(serializers.ModelSerializer):
     publisher = PublisherSerializer()
-    isbn = serializers.CharField()
-    published_at = serializers.DateTimeField()
-    description = serializers.CharField()
+
+    class Meta:
+        model = Book
+        # fields = '__all__'
+        exclude = ('cover',)
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField()
+    # isbn = serializers.CharField()
+    # published_at = serializers.DateTimeField()
+    # description = serializers.CharField()
