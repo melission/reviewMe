3b720e8f142d27d424fe7974e2da96d8f6d0ffb7
Polina <polina@blsv.space>
2021-12-10 19:00:10 +0300

    added a class based view ContributorView; modified .models and .serializers import [from a model to *].

--- reviewMe/api_views.py
+++ reviewMe/api_views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
-from books.models import Book
-from .serializers import BookSerializer
+from books.models import *
+from .serializers import *
 
 # @api_view()
 # def all_books(request):
@@ -14,3 +14,8 @@ from .serializers import BookSerializer
 class AllBooks(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class ContributorView(generics.ListAPIView):
+    queryset = Contributor.objects.get()
+    serializer_class = ContributorSerializer
