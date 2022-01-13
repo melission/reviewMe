193350477f9ef0585f02a5434ee40703148acb7e
Polina <polina@blsv.space>
2022-01-13 15:41:45 +0300

    added Login class and all necessary imports.

--- reviewMe/api_views.py
+++ reviewMe/api_views.py
@@ -6,6 +6,12 @@ from books.models import *
 from movies.models import *
 from reviews.models import ReviewBook
 from .serializers import *
+from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.views import APIView
 
 # @api_view()
 # def all_books(request):
@@ -22,6 +28,18 @@ from .serializers import *
 # class ContributorView(generics.ListAPIView):
 #     queryset = Contributor.objects.all()
 #     serializer_class = ContributorSerializer
+
+class Login(APIView):
+
+    # the default "POST" method is to be overwritten, so it will be able t provide a used with a token,
+    # which can be used in lieu of entering 'username' and 'password'. [security measures]
+    def post(self, request):
+        user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
+        if not user:
+            return Response({'error': "User doesnt' exist, or credentials are incorrect"}, status=HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=HTTP_200_OK)
+
 
 class AllBookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all()
