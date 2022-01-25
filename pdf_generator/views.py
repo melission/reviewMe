b0d2f35ed6eea6eaba699b65e8f199dc59bfd8fa
Polina <polina@blsv.space>
2022-01-25 13:31:01 +0300

    added 'optimize_size' into 'generate_pdf' function.

--- pdf_generator/views.py
+++ pdf_generator/views.py
@@ -5,7 +5,7 @@ from django.template.loader import get_template, render_to_string
 # Create your views here.
 def generate_pdf(request, model, template_name):
     html_to_string = render_to_string(template_name=template_name, context=model)
-    pdf_file = HTML(string=html_to_string).write_pdf()
+    pdf_file = HTML(string=html_to_string).write_pdf(optimize_size=('images',))
     return pdf_file
 
 
