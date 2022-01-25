92f41a7ac4bc85b49ea3a34343443ea684f93f02
Polina <polina@blsv.space>
2022-01-25 14:04:17 +0300

    added 'css' variable for rewriting styles; imported CSS property.

--- pdf_generator/views.py
+++ pdf_generator/views.py
@@ -1,11 +1,12 @@
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 from django.template.loader import get_template, render_to_string
 
 
 # Create your views here.
 def generate_pdf(request, model, template_name):
+    css = CSS(string='@page {size: landscape')
     html_to_string = render_to_string(template_name=template_name, context=model)
-    pdf_file = HTML(string=html_to_string).write_pdf(optimize_size=('images',))
+    pdf_file = HTML(string=html_to_string).write_pdf(optimize_size=('images',), stylesheets=[css])
     return pdf_file
 
 
