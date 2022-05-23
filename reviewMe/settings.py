675662c38fbf0cd07833dd23ff18c57421f96a3d
Polina <polina@blsv.space>
2022-05-23 14:50:29 +0300

    created two new classes: Dev and Prod. all initial settings were placed in Dev class.

--- reviewMe/settings.py
+++ reviewMe/settings.py
@@ -14,15 +14,7 @@ from pathlib import Path
 import json
 import os
 from django.core.exceptions import ImproperlyConfigured
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+from configurations import Configuration, values
 
 try:
     with open('sensitive.json', 'r') as f:
@@ -40,127 +32,150 @@ def get_secret(setting):
         raise ImproperlyConfigured(error_message)
 
 
-SECRET_KEY = get_secret('SECRET_KEY')
+class Dev(Configuration):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    SECRET_KEY = get_secret('SECRET_KEY')
+    DEBUG = values.BooleanValue(True)
+    TIME_ZONE = values.Value('UTC')
+    ALLOWED_HOSTS = values.ListValue([])
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-# ALLOWED_HOSTS = ['*']
-
-DEBUG = True
-ALLOWED_HOSTS = []
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+#
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+#
+# # SECURITY WARNING: keep the secret key used in production secret!
+#
+# SECRET_KEY = get_secret('SECRET_KEY')
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# # DEBUG = False
+# # ALLOWED_HOSTS = ['*']
+#
+# DEBUG = True
+#
+# ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'movies',
-    'books',
-    'reviews',
-    # 'django.contrib.admin', // was replaced with 'reviewMe.apps.ReviewMeAdminConfig'
-    # 'reviewMe.apps.ReviewMeAdminConfig', // replaced with 'reviewme_admin.apps.ReviewmeAdminConfig'
-    'reviewme_admin.apps.ReviewmeAdminConfig',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'pdf_generator',
-]
+    INSTALLED_APPS = [
+        'movies',
+        'books',
+        'reviews',
+        # 'django.contrib.admin', // was replaced with 'reviewMe.apps.ReviewMeAdminConfig'
+        # 'reviewMe.apps.ReviewMeAdminConfig', // replaced with 'reviewme_admin.apps.ReviewmeAdminConfig'
+        'reviewme_admin.apps.ReviewmeAdminConfig',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'rest_framework.authtoken',
+        'pdf_generator',
+    ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
-ROOT_URLCONF = 'reviewMe.urls'
+    ROOT_URLCONF = 'reviewMe.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
-            ],
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [BASE_DIR / 'templates']
+            ,
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'django.template.context_processors.media',
+                ],
+            },
         },
-    },
-]
+    ]
 
-WSGI_APPLICATION = 'reviewMe.wsgi.application'
+    WSGI_APPLICATION = 'reviewMe.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+    # Database
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+    # Password validation
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+    # Internationalization
+    # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-STATIC_URL = '/static/'
-# collectstatic
-STATIC_ROOT = 'production_static'
-# must be a tuple of a list
-STATICFILES_DIRS = [BASE_DIR / 'static',
-                    ("basic_static",
-                     '/Users/polinabelousova/PycharmProjects/reviewMe/reviewMe/static/reviewMe.basic.static/')]
+    LANGUAGE_CODE = 'en-us'
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+    # TIME_ZONE = 'UTC'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    USE_I18N = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
-MEDIA_URL = '/media/'
+    USE_L10N = True
+
+    USE_TZ = True
+
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+    STATIC_URL = '/static/'
+    # collectstatic
+    STATIC_ROOT = 'production_static'
+    # must be a tuple of a list
+    STATICFILES_DIRS = [BASE_DIR / 'static',
+                        ("basic_static",
+                         '/Users/polinabelousova/PycharmProjects/reviewMe/reviewMe/static/reviewMe.basic.static/')]
+
+    # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    # Default primary key field type
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+    MEDIA_URL = '/media/'
+
+
+class Prod(Dev):
+    DEBUG = False
+    SECRET_KEY = values.SecretValue()
+
