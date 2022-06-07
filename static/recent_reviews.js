786c7ddf1a7644053923a7cf58fea38f297be758
Polina <polina@blsv.space>
2022-06-07 11:56:05 +0300

    created fetchReviews function inside RecentReviews class.

--- static/recent_reviews.js
+++ static/recent_reviews.js
@@ -39,4 +39,26 @@ class RecentReviews extends React.Component {
             loading: false
         };
     }
+
+    fetchReviews() {
+        if (this.state.loading)
+            return;
+        this.setState( {loading: true});
+
+        fetch(this.state.currentUrl, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            this.setState({
+                loading: false,
+                reviews: data.results,
+                nextUrl: data.next,
+                previousUrl: data.previous,
+            })
+        })
+    }
 }
